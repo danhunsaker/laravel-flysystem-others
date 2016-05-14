cf18bb53e592cb463730361cc08466f5aaa61d98
Dan Hunsaker <danhunsaker@gmail.com>
2016-05-13 19:31:29 -0600

    Add expanded filesystem config; minor fixes
    Signed-off-by: Dan Hunsaker <danhunsaker@gmail.com>

--- src/FlysystemOtherServiceProvider.php
+++ src/FlysystemOtherServiceProvider.php
@@ -2,11 +2,11 @@
 
 namespace Danhunsaker\Laravel\Flysystem;
 
-use Log;
-use Storage;
+use Danhunsaker\Laravel\Flysystem\FlysystemServiceProvider;
 use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
-use Danhunsaker\Laravel\Flysystem\FlysystemServiceProvider;
+use Log;
+use Storage;
 
 class FlysystemOtherServiceProvider extends FlysystemServiceProvider
 {
@@ -18,79 +18,67 @@ class FlysystemOtherServiceProvider extends FlysystemServiceProvider
     public function boot()
     {
         parent::boot();
-        
-        if (class_exists('\League\Flysystem\EventableFilesystem\EventableFilesystem'))
-        {
+
+        if (class_exists('\League\Flysystem\EventableFilesystem\EventableFilesystem')) {
             $fsClass = \League\Flysystem\EventableFilesystem\EventableFilesystem::class;
-        }
-        else
-        {
+        } else {
             $fsClass = \League\Flysystem\Filesystem::class;
         }
-        
-        if (class_exists('\Zhuxiaoqiao\Flysystem\BaiduBos\BaiduBosAdapter'))
-        {
-            Storage::extend('bos', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Zhuxiaoqiao\Flysystem\BaiduBos\BaiduBosAdapter')) {
+            Storage::extend('bos', function ($app, $config) use ($fsClass) {
                 return new $fsClass(new \Zhuxiaoqiao\Flysystem\BaiduBos\BaiduBosAdapter(new \BaiduBce\Services\Bos\BosClient(Arr::except($config, ['driver', 'bucket'])), $config['bucket']));
             });
         }
-        
-        if (class_exists('\Enl\Flysystem\Cloudinary\CloudinaryAdapter'))
-        {
-            Storage::extend('cloudinary', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Enl\Flysystem\Cloudinary\CloudinaryAdapter')) {
+            Storage::extend('cloudinary', function ($app, $config) use ($fsClass) {
                 return new $fsClass(new \Enl\Flysystem\Cloudinary\CloudinaryAdapter(new \Enl\Flysystem\Cloudinary\ApiFacade($config)));
             });
-        }
-        elseif (class_exists('\T3chnik\FlysystemCloudinaryAdapter\CloudinaryAdapter'))
-        {
-            Storage::extend('cloudinary', function($app, $config) use ($fsClass) {
+        } elseif (class_exists('\T3chnik\FlysystemCloudinaryAdapter\CloudinaryAdapter')) {
+            Storage::extend('cloudinary', function ($app, $config) use ($fsClass) {
                 return new $fsClass(new \T3chnik\FlysystemCloudinaryAdapter\CloudinaryAdapter($config, new \Cloudinary\Api));
             });
         }
-        
-        if (class_exists('\Rokde\Flysystem\Adapter\LocalDatabaseAdapter'))
-        {
-            Storage::extend('eloquent', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Rokde\Flysystem\Adapter\LocalDatabaseAdapter')) {
+            Storage::extend('eloquent', function ($app, $config) use ($fsClass) {
                 return new $fsClass(new \Rokde\Flysystem\Adapter\LocalDatabaseAdapter($app->make(Arr::get($config, 'model', '\Rokde\Flysystem\Adapter\Model\FileModel'))));
             });
         }
-        
-        if (class_exists('\Litipk\Flysystem\Fallback\FallbackAdapter'))
-        {
-            Storage::extend('fallback', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Litipk\Flysystem\Fallback\FallbackAdapter')) {
+            Storage::extend('fallback', function ($app, $config) use ($fsClass) {
                 return new $fsClass(new \Litipk\Flysystem\Fallback\FallbackAdapter(Storage::disk($config['main'])->getAdapter(), Storage::disk($config['fallback'])->getAdapter()));
             });
         }
-        
-        if (class_exists('\Potherca\Flysystem\Github\GithubAdapter'))
-        {
-            Storage::extend('github', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Potherca\Flysystem\Github\GithubAdapter')) {
+            Storage::extend('github', function ($app, $config) use ($fsClass) {
                 $settings = new \Potherca\Flysystem\Github\Settings($config['project'], [\Potherca\Flysystem\Github\Settings::AUTHENTICATE_USING_TOKEN, $config['token']]);
 
                 return new $fsClass(new \Potherca\Flysystem\Github\GithubAdapter(new \Potherca\Flysystem\Github\Api(new \Github\Client(), $settings)));
             });
         }
-        
-        if (class_exists('\Ignited\Flysystem\GoogleDrive\GoogleDriveAdapter'))
-        {
-            Storage::extend('gdrive', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Ignited\Flysystem\GoogleDrive\GoogleDriveAdapter')) {
+            Storage::extend('gdrive', function ($app, $config) use ($fsClass) {
                 $client = new \Google_Client();
                 $client->setClientId($config['client_id']);
                 $client->setClientSecret($config['secret']);
                 $client->setAccessToken(json_encode([
                     "access_token" => $config['token'],
-                    "expires_in" => 3920,
-                    "token_type" => "Bearer",
-                    "created" => time()
+                    "expires_in"   => 3920,
+                    "token_type"   => "Bearer",
+                    "created"      => time()
                 ]));
-                                
+
                 return new $fsClass(new \Ignited\Flysystem\GoogleDrive\GoogleDriveAdapter(new \Google_Service_Drive($client)));
             });
         }
-        
-        if (class_exists('\Superbalist\Flysystem\GoogleStorage\GoogleStorageAdapter'))
-        {
-            Storage::extend('google', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Superbalist\Flysystem\GoogleStorage\GoogleStorageAdapter')) {
+            Storage::extend('google', function ($app, $config) use ($fsClass) {
                 $client = new \Google_Client();
                 $client->setAssertionCredentials(new \Google_Auth_AssertionCredentials(
                     $config['account'],
@@ -103,120 +91,106 @@ class FlysystemOtherServiceProvider extends FlysystemServiceProvider
                 return new $fsClass(new \Superbalist\Flysystem\GoogleStorage\GoogleStorageAdapter(new \Google_Service_Storage($client), $config['bucket']));
             });
         }
-        
-        if (class_exists('\Litipk\Flysystem\Fallback\FallbackAdapter') && class_exists('\League\Flysystem\Replicate\ReplicateAdapter'))
-        {
-            Storage::extend('mirror', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Litipk\Flysystem\Fallback\FallbackAdapter') && class_exists('\League\Flysystem\Replicate\ReplicateAdapter')) {
+            Storage::extend('mirror', function ($app, $config) use ($fsClass) {
                 return new $fsClass($this->buildMirrors($config['disks']));
             });
         }
-        
-        if (class_exists('\Ignited\Flysystem\OneDrive\OneDriveAdapter'))
-        {
-            Storage::extend('onedrive', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\JacekBarecki\FlysystemOneDrive\Adapter\OneDriveAdapter')) {
+            Storage::extend('onedrive', function ($app, $config) use ($fsClass) {
+                return new $fsClass(new \JacekBarecki\FlysystemOneDrive\Adapter\OneDriveAdapter(new \JacekBarecki\FlysystemOneDrive\Client\OneDriveClient(Arr::get($config, 'access_token'), new \GuzzleHttp\Client())));
+            });
+        } elseif (class_exists('\Ignited\Flysystem\OneDrive\OneDriveAdapter')) {
+            Storage::extend('onedrive', function ($app, $config) use ($fsClass) {
                 $oneConfig = Arr::only($config, ['base_url', 'access_token']);
-                if ($config['use_logger'])
-                {
+                if ($config['use_logger']) {
                     $logger = Log::getMonolog();
-                }
-                else
-                {
+                } else {
                     $logger = null;
                 }
-                
+
                 return new $fsClass(new \Ignited\Flysystem\OneDrive\OneDriveAdapter(\Ignited\Flysystem\OneDrive\OneDriveClient::factory($oneConfig, $logger)));
             });
         }
-        
-        if (class_exists('\Orzcc\AliyunOss\AliyunOssAdapter'))
-        {
-            Storage::extend('oss', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Orzcc\AliyunOss\AliyunOssAdapter')) {
+            Storage::extend('oss', function ($app, $config) use ($fsClass) {
                 $ossconfig = [
                     'AccessKeyId'       => $config['access_id'],
                     'AccessKeySecret'   => $config['access_key']
                 ];
-                
-                if (isset($config['endpoint']) && !empty($config['endpoint']))
+
+                if (isset($config['endpoint']) && ! empty($config['endpoint'])) {
                     $ossconfig['Endpoint'] = $config['endpoint'];
-                    
+                }
+
                 return new $fsClass(new \Orzcc\AliyunOss\AliyunOssAdapter(\Aliyun\OSS\OSSClient::factory($ossconfig), $config['bucket'], $config['prefix']));
             });
-        }
-        elseif (class_exists('\Shion\Aliyun\OSS\Adapter\OSSAdapter'))
-        {
-            Storage::extend('oss', function($app, $config) use ($fsClass) {
-                return new $fsClass(new \Shion\Aliyun\OSS\Adapter\OSSAdapter(new \Shion\Aliyun\OSS\Client\OSSClient($config), $config['bucket']));
+        } elseif (class_exists('\Shion\Aliyun\OSS\Adapter\OSSAdapter')) {
+            Storage::extend('oss', function ($app, $config) use ($fsClass) {
+                return new $fsClass(new \Shion\Aliyun\OSS\Adapter\OSSAdapter(new \Shion\Aliyun\OSS\Client\OSSClient(Arr::except($config, ['driver', 'bucket'])), $config['bucket']));
             });
         }
-        
-        if (class_exists('\EQingdan\Flysystem\Qiniu\QiniuAdapter'))
-        {
-            Storage::extend('qiniu', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\EQingdan\Flysystem\Qiniu\QiniuAdapter')) {
+            Storage::extend('qiniu', function ($app, $config) use ($fsClass) {
                 return new $fsClass(new \EQingdan\Flysystem\Qiniu\QiniuAdapter($config['accessKey'], $config['secretKey'], $config['bucket'], $config['domain']));
             });
-        }
-        elseif (class_exists('\Polev\Flysystem\Qiniu\QiniuAdapter'))
-        {
-            Storage::extend('qiniu', function($app, $config) use ($fsClass) {
+        } elseif (class_exists('\Polev\Flysystem\Qiniu\QiniuAdapter')) {
+            Storage::extend('qiniu', function ($app, $config) use ($fsClass) {
                 return new $fsClass(new \Polev\Flysystem\Qiniu\QiniuAdapter($config['accessKey'], $config['secretKey'], $config['bucket']));
             });
         }
-        
-        if (class_exists('\Danhunsaker\Flysystem\Redis\RedisAdapter'))
-        {
-            Storage::extend('redis', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Danhunsaker\Flysystem\Redis\RedisAdapter')) {
+            Storage::extend('redis', function ($app, $config) use ($fsClass) {
                 $client = $app->make('redis')->connection(Arr::get($config, 'connection', 'default'));
-                
+
                 return new $fsClass(new \Danhunsaker\Flysystem\Redis\RedisAdapter($client));
             });
         }
-        
-        if (class_exists('\Engineor\Flysystem\RunaboveAdapter'))
-        {
-            Storage::extend('runabove', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Engineor\Flysystem\RunaboveAdapter')) {
+            Storage::extend('runabove', function ($app, $config) use ($fsClass) {
                 $config['region'] = constant(\Engineor\Flysystem\Runabove::class . '::REGION_' . strtoupper($config['region']));
-                
+
                 $client = new \Engineor\Flysystem\Runabove(Arr::except($config, ['driver']));
 
                 return new $fsClass(new \Engineor\Flysystem\RunaboveAdapter($client->getContainer()));
             });
         }
-        
-        if (class_exists('\Coldwind\Filesystem\KvdbAdapter'))
-        {
-            Storage::extend('sae', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\Coldwind\Filesystem\KvdbAdapter')) {
+            Storage::extend('sae', function ($app, $config) use ($fsClass) {
                 return new $fsClass(new \Coldwind\Filesystem\KvdbAdapter(new \Coldwind\Filesystem\KvdbClient));
             });
         }
-        
-        if (class_exists('\RobGridley\Flysystem\Smb\SmbAdapter'))
-        {
-            Storage::extend('smb', function($app, $config) use ($fsClass) {
+
+        if (class_exists('\RobGridley\Flysystem\Smb\SmbAdapter')) {
+            Storage::extend('smb', function ($app, $config) use ($fsClass) {
                 $server = new \Icewind\SMB\Server($config['host'], $config['username'], $config['password']);
-                $share = $server->getShare($config['name']);
-                
+                $share = $server->getShare($config['path']);
+
                 return new $fsClass(new \RobGridley\Flysystem\Smb\SmbAdapter($share));
             });
         }
-        
-        if (class_exists('\Emgag\Flysystem\TempdirAdapter'))
-        {
-            Storage::extend('temp', function($app, $config) use ($fsClass) {
-                return new $fsClass(new \Emgag\Flysystem\TempdirAdapter($config['prefix'], $config['tempdir']));
+
+        if (class_exists('\Emgag\Flysystem\TempdirAdapter')) {
+            Storage::extend('temp', function ($app, $config) use ($fsClass) {
+                return new $fsClass(new \Emgag\Flysystem\TempdirAdapter(Arr::get($config, 'prefix'), Arr::get($config, 'tempdir')));
             });
         }
     }
-    
+
     protected function buildMirrors($disks)
     {
         $main = Storage::disk(reset($disks))->getAdapter();
 
-        if (count($disks) > 2)
-        {
+        if (count($disks) > 2) {
             $second = $this->buildMirrors(array_slice($disks, 1));
-        }
-        else
-        {
+        } else {
             $second = Storage::disk(end($disks))->getAdapter();
         }
 
@@ -230,6 +204,10 @@ class FlysystemOtherServiceProvider extends FlysystemServiceProvider
      */
     public function register()
     {
-        //
+        $source = realpath(__DIR__ . '/filesystems.php');
+
+        $this->publishes([$source => config_path('filesystems.php')]);
+
+        $this->mergeConfigFrom($source, 'filesystems');
     }
 }
---
+++ src/filesystems.php
@@ -0,0 +1,336 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default filesystem disk that should be used
+    | by the framework. A "local" driver, as well as a variety of cloud
+    | based drivers are available for your choosing. Just store away!
+    |
+    | Supported: "local", "ftp", "s3", "rackspace", "null", "azure", "copy",
+    |            "dropbox", "gridfs", "memory", "phpcr", "replicate", "sftp",
+    |            "vfs", "webdav", "zip", "bos", "cloudinary", "eloquent",
+    |            "fallback", "github", "gdrive", "google", "mirror", "onedrive",
+    |            "oss", "qiniu", "redis", "runabove", "sae", "smb", "temp"
+    |
+    */
+
+    'default' => 'local',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Cloud Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Many applications store files both locally and in the cloud. For this
+    | reason, you may specify a default "cloud" driver here. This driver
+    | will be bound as the Cloud disk implementation in the container.
+    |
+    */
+
+    'cloud' => 's3',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filesystem Disks
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure as many filesystem "disks" as you wish, and you
+    | may even configure multiple disks of the same driver. Defaults have
+    | been setup for each driver as an example of the required options.
+    |
+    */
+
+    'disks' => [
+
+        'local' => [
+            'driver'      => 'local',
+            'root'        => storage_path('app'),
+
+            // Optional Local Settings...
+            // 'permissions' => [],
+        ],
+
+        'ftp' => [
+            'driver'   => 'ftp',
+            'host'     => 'ftp.example.com',
+            'username' => 'your-username',
+            'password' => 'your-password',
+
+            // Optional FTP Settings...
+            // 'port'     => 21,
+            // 'root'     => '',
+            // 'passive'  => true,
+            // 'ssl'      => true,
+            // 'timeout'  => 30,
+        ],
+
+        's3' => [
+            'driver' => 's3',
+            'key'    => 'your-key',
+            'secret' => 'your-secret',
+            'region' => 'your-region',
+            'bucket' => 'your-bucket',
+
+            // Optional S3 Settings...
+            // 'root'   => '',
+        ],
+
+        'rackspace' => [
+            'driver'    => 'rackspace',
+            'endpoint'  => 'https://identity.api.rackspacecloud.com/v2.0/',
+            'username'  => 'your-username',
+            'key'       => 'your-key',
+            'region'    => 'IAD',
+            'url_type'  => 'publicURL',
+            'container' => 'your-container',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+
+        'azure' => [
+            'driver'      => 'azure',
+            'accountName' => 'your-account-name',
+            'apiKey'      => 'your-api-key',
+            'container'   => 'your-container',
+        ],
+
+        'copy' => [
+            'driver'         => 'copy',
+            'consumerKey'    => 'your-consumer-key',
+            'consumerSecret' => 'your-consumer-secret',
+            'accessToken'    => 'your-access-token',
+            'tokenSecret'    => 'your-token-secret',
+        ],
+
+        'dropbox' => [
+            'driver'           => 'dropbox',
+            'accessToken'      => 'your-access-token',
+            'clientIdentifier' => 'your-client-identifier',
+        ],
+
+        'gridfs' => [
+            'driver'  => 'gridfs',
+            'server'  => 'your-server',
+            'context' => 'your-context',
+            'dbName'  => 'your-db-name',
+
+            // You can also provide other MongoDB connection options here
+        ],
+
+        'memory' => [
+            'driver' => 'memory',
+        ],
+
+        'phpcr-jackrabbit' => [
+            'driver'         => 'phpcr',
+            'jackrabbit_url' => 'your-jackrabbit-url',
+            'workspace'      => 'your-workspace',
+            'root'           => 'your-root',
+
+            // Optional PHPCR Settings
+            // 'userId'         => 'your-user-id',
+            // 'password'       => 'your-password',
+        ],
+
+        'phpcr-dbal' => [
+            'driver'    => 'phpcr',
+            'database'  => 'mysql',
+            'workspace' => 'your-workspace',
+            'root'      => 'your-root',
+
+            // Optional PHPCR Settings
+            // 'userId'    => 'your-user-id',
+            // 'password'  => 'your-password',
+        ],
+
+        'phpcr-prismic' => [
+            'driver'      => 'phpcr',
+            'prismic_uri' => 'your-prismic-uri',
+            'workspace'   => 'your-workspace',
+            'root'        => 'your-root',
+
+            // Optional PHPCR Settings
+            // 'userId'      => 'your-user-id',
+            // 'password'    => 'your-password',
+        ],
+
+        'replicate' => [
+            'driver'  => 'replicate',
+            'master'  => 'local',
+            'replica' => 's3',
+        ],
+
+        'sftp' => [
+            'driver'        => 'sftp',
+            'host'          => 'sftp.example.com',
+            'username'      => 'username',
+            'password'      => 'password',
+
+            // Optional SFTP Settings
+            // 'privateKey'    => 'path/to/or/contents/of/privatekey',
+            // 'port'          => 22,
+            // 'root'          => '/path/to/root',
+            // 'timeout'       => 30,
+            // 'directoryPerm' => 0755,
+            // 'permPublic'    => 0644,
+            // 'permPrivate'   => 0600,
+        ],
+
+        'vfs' => [
+            'driver' => 'vfs',
+        ],
+
+        'webdav' => [
+            'driver'   => 'webdav',
+            'baseUri'  => 'http://example.org/dav/',
+
+            // Optional WebDAV Settings
+            // 'userName' => 'user',
+            // 'password' => 'password',
+            // 'proxy'    => 'locahost:8888',
+            // 'authType' => 'digest',  // alternately 'ntlm' or 'basic'
+            // 'encoding' => 'all',     // same as ['deflate', 'gzip', 'identity']
+        ],
+
+        'zip' => [
+            'driver' => 'zip',
+            'path'   => 'local://path/to/file.zip',
+        ],
+
+        'bos' => [
+            'driver'      => 'bos',
+            'credentials' => [
+                'ak' => 'your-access-key-id',
+                'sk' => 'your-secret-access-key',
+            ],
+            'bucket'      => 'your-bucket',
+
+            // Optional BOS Setting
+            // 'endpoint'    => 'http://bj.bcebos.com',
+        ],
+
+        'cloudinary' => [
+            'driver'     => 'cloudinary',
+            'api_key'    => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        ],
+
+        'eloquent' => [
+            'driver' => 'eloquent',
+
+            // Optional Eloquent Setting
+            // 'model'  => '\Rokde\Flysystem\Adapter\Model\FileModel',
+        ],
+
+        'fallback' => [
+            'driver'   => 'fallback',
+            'main'     => 'local',
+            'fallback' => 's3',
+        ],
+
+        'github' => [
+            'driver'  => 'github',
+            'project' => 'yourname/project',
+            'token'   => 'your-github-token',
+        ],
+
+        'gdrive' => [
+            'driver'    => 'gdrive',
+            'client_id' => 'your-client-id',
+            'secret'    => 'your-secret',
+            'token'     => 'your-token',
+        ],
+
+        'google' => [
+            'driver'        => 'google',
+            'account'       => 'your-account',
+            'secret'        => 'your-secret',
+            'developer_key' => 'your-developer-key',
+            'p12_file'      => 'local://path/to/file.p12',
+            'bucket'        => 'your-bucket',
+        ],
+
+        'mirror' => [
+            'driver' => 'mirror',
+            'disks'  => ['local', 's3', 'zip'],
+        ],
+
+        'onedrive' => [
+            'driver'       => 'onedrive',
+            'access_token' => 'your-access-token',
+
+            // Options only needed for ignited/flysystem-onedrive
+            // 'base_url'     => 'https://api.onedrive.com/v1.0/',
+            // 'use_logger'   => false,
+        ],
+
+        'oss' => [
+            'driver'     => 'oss',
+            'access_id'  => 'your-access-id',
+            'access_key' => 'your-access-key',
+            'bucket'     => 'your-bucket',
+
+            // Optional OSS Settings
+            // 'endpoint'   => '',
+            // 'prefix'     => '',
+            // 'region'     => '',    // One of 'hangzhou', 'qingdao', 'beijing', 'hongkong',
+            //                        // 'shenzhen', 'shanghai', 'west-1' and 'southeast-1'
+        ],
+
+        'qiniu' => [
+            'driver'    => 'qiniu',
+            'accessKey' => 'your-access-key',
+            'secretKey' => 'your-secret-key',
+            'bucket'    => 'your-bucket',
+
+            // Optional Qiniu Settings
+            // 'domain'    => '',
+        ],
+
+        'redis' => [
+            'driver'     => 'redis',
+            'connection' => 'default',
+        ],
+
+        'runabove' => [
+            'driver'    => 'runabove',
+            'username'  => 'your-username',
+            'password'  => 'your-password',
+            'tenantId'  => 'your-tenantId',
+
+            // Optional Runabove Settings
+            // 'container' => 'container',
+            // 'region'    => 'SBG1',   // One of 'SBG1', 'BHS1' and 'GRA1'
+        ],
+
+        'sae' => [
+            'driver' => 'sae',
+        ],
+
+        'smb' => [
+            'driver'   => 'smb',
+            'host'     => 'smb.example.com',
+            'username' => 'your-username',
+            'password' => 'your-password',
+            'path'     => 'path/to/shared/directory/for/root',
+        ],
+
+        'temp' => [
+            'driver'  => 'temp',
+
+            // Optional TempDir Settings
+            // 'prefix'  => '',
+            // 'tempdir' => '/tmp',
+        ],
+
+    ],
+
+];
