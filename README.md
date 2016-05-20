33377db963a9dbf9c9c6ece18fd3bd051a47f2b2
Dan Hunsaker <danhunsaker@gmail.com>
2016-05-19 21:34:56 -0600

    - Update README for consistency and accuracy - Add basic testing elements so Travis builds can pass - Add LICENSE file - Use `static::$fsClass`

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-## Laravel Flysystem Others
+# Laravel Flysystem Others #
 
-[![Join the chat at https://gitter.im/danhunsaker/laravel-flysystem-others](https://badges.gitter.im/danhunsaker/laravel-flysystem-others.svg)](https://gitter.im/danhunsaker/laravel-flysystem-others?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+[![Software License](https://img.shields.io/packagist/l/danhunsaker/laravel-flysystem-others.svg?style=flat-square)](LICENSE)
+[![Gitter](https://img.shields.io/gitter/room/danhunsaker/laravel-flysystem-others.svg?style=flat-square)](https://gitter.im/danhunsaker/laravel-flysystem-others)
 
-[![Total Downloads](https://poser.pugx.org/danhunsaker/laravel-flysystem-others/d/total.svg)](https://packagist.org/packages/danhunsaker/laravel-flysystem-others)
-[![Latest Stable Version](https://poser.pugx.org/danhunsaker/laravel-flysystem-others/v/stable.svg)](https://packagist.org/packages/danhunsaker/laravel-flysystem-others)
-[![Latest Unstable Version](https://poser.pugx.org/danhunsaker/laravel-flysystem-others/v/unstable.svg)](https://packagist.org/packages/danhunsaker/laravel-flysystem-others)
-[![License](https://poser.pugx.org/danhunsaker/laravel-flysystem-others/license.svg)](https://packagist.org/packages/danhunsaker/laravel-flysystem-others)
+[![Latest Stable Version](https://img.shields.io/packagist/v/danhunsaker/laravel-flysystem-others.svg?label=stable&style=flat-square)](https://github.com/danhunsaker/laravel-flysystem-others/releases)
+[![Latest Unstable Version](https://img.shields.io/packagist/vpre/danhunsaker/laravel-flysystem-others.svg?label=unstable&style=flat-square)](https://github.com/danhunsaker/laravel-flysystem-others)
+[![Build Status](https://img.shields.io/travis/danhunsaker/laravel-flysystem-others.svg?style=flat-square)](https://travis-ci.org/danhunsaker/laravel-flysystem-others)
+[![Codecov](https://img.shields.io/codecov/c/github/danhunsaker/laravel-flysystem-others.svg?style=flat-square)](https://codecov.io/gh/danhunsaker/laravel-flysystem-others)
+[![Total Downloads](https://img.shields.io/packagist/dt/danhunsaker/laravel-flysystem-others.svg?style=flat-square)](https://packagist.org/packages/danhunsaker/laravel-flysystem-others)
 
 Registers recognized third-party Flysystem adapters with Laravel automatically.
 
@@ -17,12 +19,12 @@ version of Flysystem is available, and if so, it switches to it, letting you
 listen in on Flysystem [events](http://event.thephpleague.com/) and affect them
 accordingly.
 
-> Note: This package only recognizes adapters NOT officially supported by
-> [The PHP League](https://github.com/thephpleague?query=flysystem) - but it
-> does depend on [danhunsaker/laravel-flysystem-service](https://github.com/danhunsaker/laravel-flysystem-service),
-> which does provide the official adapters.
+> Note: While this package only recognizes adapters NOT officially supported by
+> [The PHP League](https://github.com/thephpleague?query=flysystem), it *does*
+> depend on [danhunsaker/laravel-flysystem-service](https://github.com/danhunsaker/laravel-flysystem-service),
+> so installing this package *will* let you use them as well.
 
-### Installation
+## Installation ##
 
 The usual methods for using [Composer](https://getcomposer.org) apply here:
 
@@ -41,54 +43,86 @@ have to write it:
     ],
 ```
 
-Be sure to register this one *instead of*
-`Danhunsaker\Laravel\Flysystem\FlysystemServiceProvider::class`, as this one
-extends it.
+And since `FlysystemOtherServiceProvider` extends `FlysystemServiceProvider`
+from `danhunsaker/laravel-flysystem-service`, you don't need to add it as well.
+In fact, doing so will probably cause some issues with your app, as both
+providers will attempt to handle the PHP League drivers at the same time.
+
+## Setup ##
 
 For added flexibility, such as the ability to open ZIP files on remote storage,
 you can also install [twistor/flysystem-stream-wrapper](https://packagist.org/packages/twistor/flysystem-stream-wrapper),
 which will register each of the drives in your `config/filesystems.php` file as
 a stream protocol (though only when each is accessed the first time, unless you
-add them to the `autowrap` parameter in the configuration), too.  In the example
-of accessing remote ZIP files, you would then simply need to prefix the ZIP
-file's path with the name of the drive it's available on, as a URL scheme.  So
-something like `dropbox://path/to/file.zip`.
+add them to the `autowrap` parameter in the configuration).  In the example of
+accessing remote ZIP files, you would then simply need to prefix the ZIP file's
+path with the name of the drive it's available on, as a URL scheme (something
+like `dropbox://path/to/file.zip`).
 
-Finally, you can get example definitions for all supported filesystem drivers by
-running the following Artisan command:
+Finally, as with `danhunsaker/laravel-flysystem-service`, you can get example
+definitions for all supported filesystem drivers by publishing the replacement
+`filesystems` config - just run the following Artisan command:
 
 ```
 php artisan vendor:publish --provider=Danhunsaker\\Laravel\\Flysystem\\FlysystemOtherServiceProvider --force
 ```
 
-The `--force` flag is required because this will **overwrite** the existing
-`config/filesystems.php` that ships with Laravel.  You can also rename the
-existing file, then run the command without the `--force` flag, if you`d like to
-preserve the existing contents for transfer to the new file.
+The `--force` flag is required to overwrite the existing `filesystems` config
+that ships with Laravel.  You can also rename the existing file, then run the
+command without the `--force` flag, if you'd like to preserve the existing
+contents for transfer to the new file.
 
-### Supported Adapters
+## Supported Adapters ##
 
-The best place to check for which adapters are supported by this package is the Composer suggestions, but here's a quick (not-guaranteed-up-to-date) list as well:
+The best place to check for which adapters are supported by this package is the
+Composer suggestions, but here's a quick (not-guaranteed-up-to-date) list as
+well:
 
-- Aliyun OSS : [orzcc/aliyun-oss](https://packagist.org/packages/orzcc/aliyun-oss) or [shion/aliyun-oss](https://packagist.org/packages/shion/aliyun-oss)
-- Baidu Bos : [zhuxiaoqiao/flysystem-baidu-bos](https://packagist.org/packages/zhuxiaoqiao/flysystem-baidu-bos)
-- Cloudinary: [enl/flysystem-cloudinary](https://packagist.org/packages/enl/flysystem-cloudinary) or [t3chnik/flysystem-cloudinary-adapter](https://packagist.org/packages/t3chnik/flysystem-cloudinary-adapter)
-- Eloquent: [rokde/flysystem-local-database-adapter](https://packagist.org/packages/rokde/flysystem-local-database-adapter)
-- Fallback: [litipk/flysystem-fallback-adapter](https://packagist.org/packages/litipk/flysystem-fallback-adapter)
-- GitHub: [potherca/flysystem-github](https://packagist.org/packages/potherca/flysystem-github)
-- Google Cloud Storage: [superbalist/flysystem-google-storage](https://packagist.org/packages/superbalist/flysystem-google-storage)
-- Google Drive: [ignited/flysystem-google-drive](https://packagist.org/packages/ignited/flysystem-google-drive)
-- Mirror: A "meta-adapter" which combines the Fallback and Replicate adapters, if both are available.
-- OneDrive: [jacekbarecki/flysystem-onedrive](https://packagist.org/packages/jacekbarecki/flysystem-onedrive) or [ignited/flysystem-onedrive](https://packagist.org/packages/ignited/flysystem-onedrive)
-- Qiniu: [eqingdan/flysystem-qiniu](https://packagist.org/packages/eqingdan/flysystem-qiniu) or [polev/flysystem-qiniu](https://packagist.org/packages/polev/flysystem-qiniu)
-- Redis: [danhunsaker/flysystem-redis](https://packagist.org/packages/danhunsaker/flysystem-redis)
-- Runabove: [engineor/flysystem-runabove](https://packagist.org/packages/engineor/flysystem-runabove)
-- Sae: [coldwind/flysystem-sae](https://packagist.org/packages/coldwind/flysystem-sae)
-- SMB/CIFS: [robgridley/flysystem-smb](https://packagist.org/packages/robgridley/flysystem-smb)
-- Temp: [emgag/flysystem-tempdir](https://packagist.org/packages/emgag/flysystem-tempdir)
+- Aliyun OSS:
+  [orzcc/aliyun-oss](https://packagist.org/packages/orzcc/aliyun-oss)
+  or [shion/aliyun-oss](https://packagist.org/packages/shion/aliyun-oss)
+- Baidu Bos:
+  [zhuxiaoqiao/flysystem-baidu-bos](https://packagist.org/packages/zhuxiaoqiao/flysystem-baidu-bos)
+- Cloudinary:
+  [enl/flysystem-cloudinary](https://packagist.org/packages/enl/flysystem-cloudinary)
+  or [t3chnik/flysystem-cloudinary-adapter](https://packagist.org/packages/t3chnik/flysystem-cloudinary-adapter)
+- Eloquent:
+  [rokde/flysystem-local-database-adapter](https://packagist.org/packages/rokde/flysystem-local-database-adapter)
+- Fallback:
+  [litipk/flysystem-fallback-adapter](https://packagist.org/packages/litipk/flysystem-fallback-adapter)
+- GitHub:
+  [potherca/flysystem-github](https://packagist.org/packages/potherca/flysystem-github)
+- Google Cloud Storage:
+  [superbalist/flysystem-google-storage](https://packagist.org/packages/superbalist/flysystem-google-storage)
+- Google Drive:
+  [ignited/flysystem-google-drive](https://packagist.org/packages/ignited/flysystem-google-drive)
+- Mirror:
+  A "meta-adapter" which combines the Fallback and Replicate adapters,
+  if both are available.
+- OneDrive:
+  [jacekbarecki/flysystem-onedrive](https://packagist.org/packages/jacekbarecki/flysystem-onedrive)
+  or [ignited/flysystem-onedrive](https://packagist.org/packages/ignited/flysystem-onedrive)
+- Qiniu:
+  [eqingdan/flysystem-qiniu](https://packagist.org/packages/eqingdan/flysystem-qiniu)
+  or [polev/flysystem-qiniu](https://packagist.org/packages/polev/flysystem-qiniu)
+- Redis:
+  [danhunsaker/flysystem-redis](https://packagist.org/packages/danhunsaker/flysystem-redis)
+- Runabove:
+  [engineor/flysystem-runabove](https://packagist.org/packages/engineor/flysystem-runabove)
+- Sae:
+  [coldwind/flysystem-sae](https://packagist.org/packages/coldwind/flysystem-sae)
+- SMB/CIFS:
+  [robgridley/flysystem-smb](https://packagist.org/packages/robgridley/flysystem-smb)
+- Temp:
+  [emgag/flysystem-tempdir](https://packagist.org/packages/emgag/flysystem-tempdir)
 
-### Issues, Contributions, Etc
+## Issues, Contributions, Etc ##
 
-GitHub is the best place to interact about this project.
+Pull requests, bug reports, and so forth are all welcome on [GitHub][].
 
-Enjoy!
+Security issues should be reported directly to [danhunsaker (plus) laraflyplus
+(at) gmail (dot) com](mailto:danhunsaker+laraflyplus@gmail.com).
+
+And head to [GitHub][] for everything else.
+
+[GitHub]:https://github.com/danhunsaker/laravel-flysystem-others
--- src/FlysystemOtherServiceProvider.php
+++ src/FlysystemOtherServiceProvider.php
@@ -20,50 +20,44 @@ class FlysystemOtherServiceProvider extends FlysystemServiceProvider
     {
         parent::boot();
 
-        if (class_exists('\League\Flysystem\EventableFilesystem\EventableFilesystem')) {
-            $fsClass = \League\Flysystem\EventableFilesystem\EventableFilesystem::class;
-        } else {
-            $fsClass = \League\Flysystem\Filesystem::class;
-        }
-
         if (class_exists('\Zhuxiaoqiao\Flysystem\BaiduBos\BaiduBosAdapter')) {
-            Storage::extend('bos', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \Zhuxiaoqiao\Flysystem\BaiduBos\BaiduBosAdapter(new \BaiduBce\Services\Bos\BosClient(Arr::except($config, ['driver', 'bucket'])), $config['bucket']));
+            Storage::extend('bos', function ($app, $config) {
+                return new static::$fsClass(new \Zhuxiaoqiao\Flysystem\BaiduBos\BaiduBosAdapter(new \BaiduBce\Services\Bos\BosClient(Arr::except($config, ['driver', 'bucket'])), $config['bucket']));
             });
         }
 
         if (class_exists('\Enl\Flysystem\Cloudinary\CloudinaryAdapter')) {
-            Storage::extend('cloudinary', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \Enl\Flysystem\Cloudinary\CloudinaryAdapter(new \Enl\Flysystem\Cloudinary\ApiFacade($config)));
+            Storage::extend('cloudinary', function ($app, $config) {
+                return new static::$fsClass(new \Enl\Flysystem\Cloudinary\CloudinaryAdapter(new \Enl\Flysystem\Cloudinary\ApiFacade($config)));
             });
         } elseif (class_exists('\T3chnik\FlysystemCloudinaryAdapter\CloudinaryAdapter')) {
-            Storage::extend('cloudinary', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \T3chnik\FlysystemCloudinaryAdapter\CloudinaryAdapter($config, new \Cloudinary\Api));
+            Storage::extend('cloudinary', function ($app, $config) {
+                return new static::$fsClass(new \T3chnik\FlysystemCloudinaryAdapter\CloudinaryAdapter($config, new \Cloudinary\Api));
             });
         }
 
         if (class_exists('\Rokde\Flysystem\Adapter\LocalDatabaseAdapter')) {
-            Storage::extend('eloquent', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \Rokde\Flysystem\Adapter\LocalDatabaseAdapter($app->make(Arr::get($config, 'model', '\Rokde\Flysystem\Adapter\Model\FileModel'))));
+            Storage::extend('eloquent', function ($app, $config) {
+                return new static::$fsClass(new \Rokde\Flysystem\Adapter\LocalDatabaseAdapter($app->make(Arr::get($config, 'model', '\Rokde\Flysystem\Adapter\Model\FileModel'))));
             });
         }
 
         if (class_exists('\Litipk\Flysystem\Fallback\FallbackAdapter')) {
-            Storage::extend('fallback', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \Litipk\Flysystem\Fallback\FallbackAdapter(Storage::disk($config['main'])->getAdapter(), Storage::disk($config['fallback'])->getAdapter()));
+            Storage::extend('fallback', function ($app, $config) {
+                return new static::$fsClass(new \Litipk\Flysystem\Fallback\FallbackAdapter(Storage::disk($config['main'])->getAdapter(), Storage::disk($config['fallback'])->getAdapter()));
             });
         }
 
         if (class_exists('\Potherca\Flysystem\Github\GithubAdapter')) {
-            Storage::extend('github', function ($app, $config) use ($fsClass) {
+            Storage::extend('github', function ($app, $config) {
                 $settings = new \Potherca\Flysystem\Github\Settings($config['project'], [\Potherca\Flysystem\Github\Settings::AUTHENTICATE_USING_TOKEN, $config['token']]);
 
-                return new $fsClass(new \Potherca\Flysystem\Github\GithubAdapter(new \Potherca\Flysystem\Github\Api(new \Github\Client(), $settings)));
+                return new static::$fsClass(new \Potherca\Flysystem\Github\GithubAdapter(new \Potherca\Flysystem\Github\Api(new \Github\Client(), $settings)));
             });
         }
 
         if (class_exists('\Ignited\Flysystem\GoogleDrive\GoogleDriveAdapter')) {
-            Storage::extend('gdrive', function ($app, $config) use ($fsClass) {
+            Storage::extend('gdrive', function ($app, $config) {
                 $client = new \Google_Client();
                 $client->setClientId($config['client_id']);
                 $client->setClientSecret($config['secret']);
@@ -74,12 +68,12 @@ class FlysystemOtherServiceProvider extends FlysystemServiceProvider
                     "created"      => time()
                 ]));
 
-                return new $fsClass(new \Ignited\Flysystem\GoogleDrive\GoogleDriveAdapter(new \Google_Service_Drive($client)));
+                return new static::$fsClass(new \Ignited\Flysystem\GoogleDrive\GoogleDriveAdapter(new \Google_Service_Drive($client)));
             });
         }
 
         if (class_exists('\Superbalist\Flysystem\GoogleStorage\GoogleStorageAdapter')) {
-            Storage::extend('google', function ($app, $config) use ($fsClass) {
+            Storage::extend('google', function ($app, $config) {
                 $client = new \Google_Client();
                 $client->setAssertionCredentials(new \Google_Auth_AssertionCredentials(
                     $config['account'],
@@ -89,22 +83,22 @@ class FlysystemOtherServiceProvider extends FlysystemServiceProvider
                 ));
                 $client->setDeveloperKey($config['developer_key']);
 
-                return new $fsClass(new \Superbalist\Flysystem\GoogleStorage\GoogleStorageAdapter(new \Google_Service_Storage($client), $config['bucket']));
+                return new static::$fsClass(new \Superbalist\Flysystem\GoogleStorage\GoogleStorageAdapter(new \Google_Service_Storage($client), $config['bucket']));
             });
         }
 
         if (class_exists('\Litipk\Flysystem\Fallback\FallbackAdapter') && class_exists('\League\Flysystem\Replicate\ReplicateAdapter')) {
-            Storage::extend('mirror', function ($app, $config) use ($fsClass) {
-                return new $fsClass($this->buildMirrors($config['disks']));
+            Storage::extend('mirror', function ($app, $config) {
+                return new static::$fsClass($this->buildMirrors($config['disks']));
             });
         }
 
         if (class_exists('\JacekBarecki\FlysystemOneDrive\Adapter\OneDriveAdapter')) {
-            Storage::extend('onedrive', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \JacekBarecki\FlysystemOneDrive\Adapter\OneDriveAdapter(new \JacekBarecki\FlysystemOneDrive\Client\OneDriveClient(Arr::get($config, 'access_token'), new \GuzzleHttp\Client())));
+            Storage::extend('onedrive', function ($app, $config) {
+                return new static::$fsClass(new \JacekBarecki\FlysystemOneDrive\Adapter\OneDriveAdapter(new \JacekBarecki\FlysystemOneDrive\Client\OneDriveClient(Arr::get($config, 'access_token'), new \GuzzleHttp\Client())));
             });
         } elseif (class_exists('\Ignited\Flysystem\OneDrive\OneDriveAdapter')) {
-            Storage::extend('onedrive', function ($app, $config) use ($fsClass) {
+            Storage::extend('onedrive', function ($app, $config) {
                 $oneConfig = Arr::only($config, ['base_url', 'access_token']);
                 if ($config['use_logger']) {
                     $logger = Log::getMonolog();
@@ -112,12 +106,12 @@ class FlysystemOtherServiceProvider extends FlysystemServiceProvider
                     $logger = null;
                 }
 
-                return new $fsClass(new \Ignited\Flysystem\OneDrive\OneDriveAdapter(\Ignited\Flysystem\OneDrive\OneDriveClient::factory($oneConfig, $logger)));
+                return new static::$fsClass(new \Ignited\Flysystem\OneDrive\OneDriveAdapter(\Ignited\Flysystem\OneDrive\OneDriveClient::factory($oneConfig, $logger)));
             });
         }
 
         if (class_exists('\Orzcc\AliyunOss\AliyunOssAdapter')) {
-            Storage::extend('oss', function ($app, $config) use ($fsClass) {
+            Storage::extend('oss', function ($app, $config) {
                 $ossconfig = [
                     'AccessKeyId'       => $config['access_id'],
                     'AccessKeySecret'   => $config['access_key']
@@ -127,72 +121,72 @@ class FlysystemOtherServiceProvider extends FlysystemServiceProvider
                     $ossconfig['Endpoint'] = $config['endpoint'];
                 }
 
-                return new $fsClass(new \Orzcc\AliyunOss\AliyunOssAdapter(\Aliyun\OSS\OSSClient::factory($ossconfig), $config['bucket'], $config['prefix']));
+                return new static::$fsClass(new \Orzcc\AliyunOss\AliyunOssAdapter(\Aliyun\OSS\OSSClient::factory($ossconfig), $config['bucket'], $config['prefix']));
             });
         } elseif (class_exists('\Shion\Aliyun\OSS\Adapter\OSSAdapter')) {
-            Storage::extend('oss', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \Shion\Aliyun\OSS\Adapter\OSSAdapter(new \Shion\Aliyun\OSS\Client\OSSClient(Arr::except($config, ['driver', 'bucket'])), $config['bucket']));
+            Storage::extend('oss', function ($app, $config) {
+                return new static::$fsClass(new \Shion\Aliyun\OSS\Adapter\OSSAdapter(new \Shion\Aliyun\OSS\Client\OSSClient(Arr::except($config, ['driver', 'bucket'])), $config['bucket']));
             });
         }
 
         if (class_exists('\EQingdan\Flysystem\Qiniu\QiniuAdapter')) {
-            Storage::extend('qiniu', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \EQingdan\Flysystem\Qiniu\QiniuAdapter($config['accessKey'], $config['secretKey'], $config['bucket'], $config['domain']));
+            Storage::extend('qiniu', function ($app, $config) {
+                return new static::$fsClass(new \EQingdan\Flysystem\Qiniu\QiniuAdapter($config['accessKey'], $config['secretKey'], $config['bucket'], $config['domain']));
             });
         } elseif (class_exists('\Polev\Flysystem\Qiniu\QiniuAdapter')) {
-            Storage::extend('qiniu', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \Polev\Flysystem\Qiniu\QiniuAdapter($config['accessKey'], $config['secretKey'], $config['bucket']));
+            Storage::extend('qiniu', function ($app, $config) {
+                return new static::$fsClass(new \Polev\Flysystem\Qiniu\QiniuAdapter($config['accessKey'], $config['secretKey'], $config['bucket']));
             });
         }
 
         if (class_exists('\Danhunsaker\Flysystem\Redis\RedisAdapter')) {
-            Storage::extend('redis', function ($app, $config) use ($fsClass) {
+            Storage::extend('redis', function ($app, $config) {
                 $client = $app->make('redis')->connection(Arr::get($config, 'connection', 'default'));
 
-                return new $fsClass(new \Danhunsaker\Flysystem\Redis\RedisAdapter($client));
+                return new static::$fsClass(new \Danhunsaker\Flysystem\Redis\RedisAdapter($client));
             });
         }
 
         if (class_exists('\Engineor\Flysystem\RunaboveAdapter')) {
-            Storage::extend('runabove', function ($app, $config) use ($fsClass) {
+            Storage::extend('runabove', function ($app, $config) {
                 $config['region'] = constant(\Engineor\Flysystem\Runabove::class . '::REGION_' . strtoupper($config['region']));
 
                 $client = new \Engineor\Flysystem\Runabove(Arr::except($config, ['driver']));
 
-                return new $fsClass(new \Engineor\Flysystem\RunaboveAdapter($client->getContainer()));
+                return new static::$fsClass(new \Engineor\Flysystem\RunaboveAdapter($client->getContainer()));
             });
         }
 
         if (class_exists('\Coldwind\Filesystem\KvdbAdapter')) {
-            Storage::extend('sae', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \Coldwind\Filesystem\KvdbAdapter(new \Coldwind\Filesystem\KvdbClient));
+            Storage::extend('sae', function ($app, $config) {
+                return new static::$fsClass(new \Coldwind\Filesystem\KvdbAdapter(new \Coldwind\Filesystem\KvdbClient));
             });
         }
 
         if (class_exists('\RobGridley\Flysystem\Smb\SmbAdapter')) {
-            Storage::extend('smb', function ($app, $config) use ($fsClass) {
+            Storage::extend('smb', function ($app, $config) {
                 $server = new \Icewind\SMB\Server($config['host'], $config['username'], $config['password']);
                 $share = $server->getShare($config['path']);
 
-                return new $fsClass(new \RobGridley\Flysystem\Smb\SmbAdapter($share));
+                return new static::$fsClass(new \RobGridley\Flysystem\Smb\SmbAdapter($share));
             });
         }
 
         if (class_exists('\Emgag\Flysystem\TempdirAdapter')) {
-            Storage::extend('temp', function ($app, $config) use ($fsClass) {
-                return new $fsClass(new \Emgag\Flysystem\TempdirAdapter(Arr::get($config, 'prefix'), Arr::get($config, 'tempdir')));
+            Storage::extend('temp', function ($app, $config) {
+                return new static::$fsClass(new \Emgag\Flysystem\TempdirAdapter(Arr::get($config, 'prefix'), Arr::get($config, 'tempdir')));
             });
         }
     }
 
     protected function buildMirrors($disks)
     {
-        $main = Storage::disk(reset($disks))->getAdapter();
+        $main = Storage::disk(Arr::first($disks))->getAdapter();
 
         if (count($disks) > 2) {
             $second = $this->buildMirrors(array_slice($disks, 1));
         } else {
-            $second = Storage::disk(end($disks))->getAdapter();
+            $second = Storage::disk(Arr::last($disks))->getAdapter();
         }
 
         return new \League\Flysystem\Replicate\ReplicateAdapter(new \Litipk\Flysystem\Fallback\FallbackAdapter($main, $second, true), $second);
@@ -205,11 +199,9 @@ class FlysystemOtherServiceProvider extends FlysystemServiceProvider
      */
     public function register()
     {
-        $source = realpath(__DIR__ . '/filesystems.php');
+        $source = realpath(__DIR__ . '../config/filesystems.php');
 
         $this->publishes([$source => config_path('filesystems.php')]);
-
-        $this->mergeConfigFrom($source, 'filesystems');
 
         if (class_exists('Twistor\FlysystemStreamWrapper')) {
             App::register('Danhunsaker\Laravel\Flysystem\FlysystemStreamWrapperServiceProvider');
